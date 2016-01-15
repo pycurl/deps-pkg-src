64c3996786955d14a49b3be26b27592596ae8bc3
Oleg Pudeyev <oleg@bsdpower.com>
2016-01-15 01:29:38 -0500

    DRY

---
+++ inc/lib.sh
@@ -0,0 +1,28 @@
+wget_once() {
+  url="$1"
+  if ! test -f `basename "$url"`; then
+    wget -O `basename "$url"`.part "$url"
+    rv=$?
+    if test $rv = 0; then
+      mv `basename "$url"`.part `basename "$url"`
+    else
+      rm -f `basename "$url"`.part
+      return $rv
+    fi
+  fi
+}
+
+wget_once_to() {
+  url="$1"
+  dest="$2"
+  if ! test -f "$dest"; then
+    wget -O "$dest".part "$url"
+    rv=$?
+    if test $rv = 0; then
+      mv "$dest".part "$dest"
+    else
+      rm -f "$dest".part
+      return $rv
+    fi
+  fi
+}
